43e439f5506aaa35bd89afc1ec74a6a942b6e8fd
Pablo Schaffner <57605485+puntorigen@users.noreply.github.com>
2024-06-04 16:16:38 -0400

    - fix pydatic parser attempt

--- src/crewai/utilities/pydantic_schema_parser.py
+++ src/crewai/utilities/pydantic_schema_parser.py
@@ -8,38 +8,37 @@ class PydanticSchemaParser(BaseModel):
     def get_schema(self) -> str:
         """
         Public method to get the schema of a Pydantic model.
-        
-        :param model: The Pydantic model class to generate schema for.
         :return: String representation of the model schema.
         """
-        return self._get_model_schema(self.model)
+        return "{\n" + self._get_model_schema(self.model) + "\n}"
 
     def _get_model_schema(self, model: Type[BaseModel], depth=0) -> str:
         lines = []
-        for field_name, field_info in model.__fields__.items():
+        indent = ' ' * 4 * (depth + 1)
+        for field_name, field_info in model.model_fields.items():
             field_type = field_info.type_
             field_type_str = self._get_field_type(field_type, depth + 1)
-            lines.append(f"{' ' * 4 * depth}- {field_name}: {field_type_str}")
+            lines.append(f"{indent}'{field_name}': {field_type_str}")
 
-        return "\n".join(lines)
+        return ",\n".join(lines)
 
     def _get_field_type(self, field_type, depth) -> str:
         origin = get_origin(field_type)
         args = get_args(field_type)
 
-        if origin is list:  # Handles List[x]
+        if origin is list:
             item_type = args[0]
             if isinstance(item_type, type) and issubclass(item_type, BaseModel):
                 nested_schema = self._get_model_schema(item_type, depth + 1)
-                return f"List[\n{nested_schema}\n{' ' * 4 * depth}]"
+                return f"[{{\n{nested_schema}\n{' ' * 4 * depth}}}]"
             else:
-                return f"List[{item_type.__name__}]"
-        elif origin is Union:  # Handles Union[x, y] and Optional[x]
-            types = ", ".join(self._get_field_type(arg, depth) for arg in args if arg is not type(None))
+                return f"[{item_type.__name__}]"
+        elif origin is Union:
+            types = ", ".join([self._get_field_type(arg, depth) for arg in args if arg is not type(None)])
             return f"Union[{types}]"
         elif isinstance(field_type, type) and issubclass(field_type, BaseModel):
-            return f"\n{self._get_model_schema(field_type, depth)}"
+            return f"{{\n{self._get_model_schema(field_type, depth)}\n{' ' * 4 * depth}}}"
         elif isinstance(field_type, type):
-            return field_type.__name__
+            return f"'{field_type.__name__}'"
         else:
-            return str(field_type)  # Fallback for other complex types
+            return f"'{str(field_type)}'"  # Fallback for other complex types
